a7e954ab2c10eacf3bc92bdeec72b1f4dca67b6b
Eric Woods <woodseowl@gmail.com>
2018-02-04 23:13:39 -0500

    Donation form submits to Stripe

--- web/modules/custom/lbw_donation/src/Form/DonationForm.php
+++ web/modules/custom/lbw_donation/src/Form/DonationForm.php
@@ -30,10 +30,49 @@ class DonationForm extends FormBase {
             '#template' => $this->getFieldTemplate('description'),
         ];
 
-        $form['stripe-checkout'] = [
+        $form['donation_amount'] = [
+            '#type' => 'number',
+            '#title' => 'Amount',
+            '#value' => '1000',
+        ];
+
+        $form['stripe_checkout'] = [
             '#type' => 'inline_template',
             '#template' => $this->getFieldTemplate('stripe-checkout'),
             '#context' => $this->getSwipeCheckoutContext(),
+        ];
+
+        $form['stripeToken'] = [
+            '#type' => 'hidden'
+        ];
+
+        $form['stripeEmail'] = [
+            '#type' => 'hidden'
+        ];
+
+        $form['stripeBillingName'] = [
+            '#type' => 'hidden'
+        ];
+
+        $form['stripeBillingAddressLine1'] = [
+            '#type' => 'hidden'
+        ];
+
+        $form['stripeBillingAddressCity'] = [
+            '#type' => 'hidden'
+        ];
+
+        $form['stripeBillingAddressState'] = [
+            '#type' => 'hidden'
+        ];
+
+        $form['stripeBillingAddressZip'] = [
+            '#type' => 'hidden'
+        ];
+
+        $form['submit'] = [
+            '#type' => 'submit',
+            '#value' => $this->t('Submit'),
         ];
 
         return $form;
@@ -55,10 +94,14 @@ class DonationForm extends FormBase {
             drupal_set_message($key . ': ' . $value);
         }
 
+        $this->processCharge($form_state);
+
     }
 
     /**
      * @param $field
+     *
+     * @return bool|string
      */
     private function getFieldTemplate($field)
     {
@@ -70,15 +113,45 @@ class DonationForm extends FormBase {
 
     private function getSwipeCheckoutContext()
     {
-        $stripe_config = \Drupal::config('stripe.settings');
-        $stripe_environment = $stripe_config->get('environment');
-
         $variables = [
+            'stripe_key' => $this->getStripeKey('public'),
             'site_name' => \Drupal::config('system.site')->get('name'),
-            'stripe_key' => $stripe_config->get("apikey.{$stripe_environment}.public"),
+            'description' => 'Donation',
+            'amount' => '1000',
         ];
 
         return $variables;
+    }
+
+    /**
+     * @param $key
+     * @return array|mixed|null
+     */
+    private function getStripeKey($key)
+    {
+        $stripe_config = \Drupal::config('stripe.settings');
+        $stripe_environment = $stripe_config->get('environment');
+        $stripe_public_key = $stripe_config->get("apikey.{$stripe_environment}.{$key}");
+
+        return $stripe_public_key;
+    }
+
+    /**
+     * @param FormStateInterface $form_state
+     */
+    private function processCharge(FormStateInterface $form_state)
+    {
+        \Stripe\Stripe::setApiKey($this->getStripeKey('secret'));
+
+        $stripe_token = $form_state->getValue('stripeToken');
+        $donation_amount = $form_state->getValue('donation_amount');
+
+        $charge = \Stripe\Charge::create([
+            'amount' => $donation_amount,
+            'currency' => 'usd',
+            'description' => 'Donation',
+            'source' => $stripe_token,
+        ]);
     }
 
 }
